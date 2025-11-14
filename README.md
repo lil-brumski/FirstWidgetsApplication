f2e452583e04dfce610757deae06789b6869ebb9
lil-brumski <davidbrume7@gmail.com>
2025-11-14 20:41:06 +0100

    Track my Qt6 learning.

---
+++ README.md
@@ -0,0 +1,9 @@
+# FirstWidgetsApplication
+
+This repo is for tracking my learning of the Qt Framework.
+
+![Demo](media/LearningQWidgets.gif)
+
+This project uses the following third party libraries:
+
++ Qt6 - [LGPL](https://doc.qt.io/qt-6/lgpl.html)
---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <QApplication>
+#include <QPushButton>
+#include <QLabel>
+#include <QLineEdit>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QList>
+
+int main(int argc, char** argv) {
+	QApplication app(argc, argv);
+
+	QWidget window;
+	window.resize(1024, 600);
+	window.setWindowTitle(QApplication::translate("hi", "Brumski's First Qt6 Application"));
+
+	QList<QPushButton*> buttons = {
+		new QPushButton(QApplication::translate("hi", "Submit")),
+		new QPushButton(QApplication::translate("hi", "Submit")),
+		new QPushButton(QApplication::translate("hi", "Submit"))
+	};
+	
+	QList<QLabel*> listOfLabels = {
+		new QLabel("Name"),
+		new QLabel("Department"),
+		new QLabel("Matriculation Number")
+	};
+	
+	QList<QLineEdit*> LElists = {
+		new QLineEdit(),
+		new QLineEdit(),
+		new QLineEdit()
+	}; 
+
+	LElists[0]->setPlaceholderText(QApplication::translate("hi", "Enter your name"));
+	LElists[1]->setPlaceholderText(QApplication::translate("hi", "Enter your department"));
+	LElists[2]->setPlaceholderText(QApplication::translate("hi", "Enter your matric number"));
+
+	QHBoxLayout* LO1 = new QHBoxLayout();
+	LO1->addWidget(listOfLabels[0]);
+	LO1->addWidget(LElists[0]);
+	LO1->addWidget(buttons[0]);
+
+	QHBoxLayout* LO2 = new QHBoxLayout();
+	LO2->addWidget(listOfLabels[1]);
+	LO2->addWidget(LElists[1]);
+	LO2->addWidget(buttons[1]);
+
+	QHBoxLayout* LO3 = new QHBoxLayout();
+	LO3->addWidget(listOfLabels[2]);
+	LO3->addWidget(LElists[2]);
+	LO3->addWidget(buttons[2]);
+
+	QVBoxLayout* LOMAIN = new QVBoxLayout();
+	LOMAIN->addLayout(LO1);
+	LOMAIN->addLayout(LO2);
+	LOMAIN->addLayout(LO3);
+	
+	window.setLayout(LOMAIN);
+	window.show();
+
+	return app.exec();
+}
